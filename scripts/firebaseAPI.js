1b5353d8d2886da4ab0dad7b6601565607f11d2b
Ryan Toews <rtoews11@my.bcit.ca>
2021-11-09 08:29:10 -0800

    completed lab09

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyC9f_fqYvT0fftK4OHeMUIpDm6xnVC_TK8",
-  authDomain: "mrburger-toews-ryan.firebaseapp.com",
-  projectId: "mrburger-toews-ryan",
-  storageBucket: "mrburger-toews-ryan.appspot.com",
-  messagingSenderId: "353018993953",
-  appId: "1:353018993953:web:bbe0baa894fea1f62fb260"
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: 
 };
   
 
